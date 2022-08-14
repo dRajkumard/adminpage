85a48098ac7c8621b4487dd9828b38cb2b5cd6e8
unknown <rajkumar.d408@gmail.com>
2022-08-14 22:19:29 +0530

    chages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+// import {} from "react-router-dom"
+import Login from './components/Loginpage/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Login/>
+    </div>
   )
 }
 
